1ca59b3cb09d3bcf424ee66a78bd2eb191c70d16
Martijn Laarman <Mpdreamz@gmail.com>
2020-05-15 19:38:32 +0200

    code reformat

--- src/assembly-rewriter/AssemblyResolver.cs
+++ src/assembly-rewriter/AssemblyResolver.cs
@@ -6,30 +6,30 @@ using Mono.Cecil;
 
 namespace AssemblyRewriter
 {
-    internal class AssemblyResolver : DefaultAssemblyResolver
-    {
-        private readonly IEnumerable<string> _directories;
+	internal class AssemblyResolver : DefaultAssemblyResolver
+	{
+		private readonly IEnumerable<string> _directories;
 
-        public AssemblyResolver(IEnumerable<string> directories) =>
-            _directories = new HashSet<string>(directories.Select(Path.GetFullPath), StringComparer.OrdinalIgnoreCase);
+		public AssemblyResolver(IEnumerable<string> directories) =>
+			_directories = new HashSet<string>(directories.Select(Path.GetFullPath), StringComparer.OrdinalIgnoreCase);
 
-        public override AssemblyDefinition Resolve(AssemblyNameReference name)
-        {
-            try
-            {
-                return base.Resolve(name);
-            }
-            catch
-            {
-                foreach (var directory in _directories)
-                {
-                    var filePath = Path.Combine(directory, name.Name + ".dll");
-                    if (File.Exists(filePath))
-                        return AssemblyDefinition.ReadAssembly(filePath);
-                }
+		public override AssemblyDefinition Resolve(AssemblyNameReference name)
+		{
+			try
+			{
+				return base.Resolve(name);
+			}
+			catch
+			{
+				foreach (var directory in _directories)
+				{
+					var filePath = Path.Combine(directory, name.Name + ".dll");
+					if (File.Exists(filePath))
+						return AssemblyDefinition.ReadAssembly(filePath);
+				}
 
-                throw;
-            }
-        }
-    }
+				throw;
+			}
+		}
+	}
 }
--- src/assembly-rewriter/AssemblyToRewrite.cs
+++ src/assembly-rewriter/AssemblyToRewrite.cs
@@ -2,31 +2,31 @@
 
 namespace AssemblyRewriter
 {
-    internal class AssemblyToRewrite
-    {
-        private string _inputName;
-        private string _outputName;
-        private string _inputDirectory;
-        private string _outputDirectory;
+	internal class AssemblyToRewrite
+	{
+		private string _inputDirectory;
+		private string _inputName;
+		private string _outputDirectory;
+		private string _outputName;
 
-        public AssemblyToRewrite(string inputPath, string outputPath)
-        {
-            InputPath = Path.GetFullPath(inputPath);
-            OutputPath = Path.GetFullPath(outputPath);
-        }
+		public AssemblyToRewrite(string inputPath, string outputPath)
+		{
+			InputPath = Path.GetFullPath(inputPath);
+			OutputPath = Path.GetFullPath(outputPath);
+		}
 
-        public string InputPath { get; }
+		public string InputPath { get; }
 
-        public string InputDirectory => _inputDirectory ?? (_inputDirectory = Path.GetDirectoryName(InputPath));
+		public string InputDirectory => _inputDirectory ??= Path.GetDirectoryName(InputPath);
 
-        public string InputName => _inputName ?? (_inputName = Path.GetFileNameWithoutExtension(InputPath));
+		public string InputName => _inputName ??= Path.GetFileNameWithoutExtension(InputPath);
 
-        public string OutputDirectory => _outputDirectory ?? (_outputDirectory = Path.GetDirectoryName(OutputPath));
+		public string OutputDirectory => _outputDirectory ??= Path.GetDirectoryName(OutputPath);
 
-        public string OutputName => _outputName ?? (_outputName = Path.GetFileNameWithoutExtension(OutputPath));
+		public string OutputName => _outputName ??= Path.GetFileNameWithoutExtension(OutputPath);
 
-        public string OutputPath { get; }
+		public string OutputPath { get; }
 
-        public bool Rewritten { get; set; }
-    }
+		public bool Rewritten { get; set; }
+	}
 }
